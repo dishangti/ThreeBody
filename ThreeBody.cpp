b2be84077ba43c6211a4fc7e42c39654f2b4091b
dishangti <16698219+dishangti@users.noreply.github.com>
2023-06-25 19:27:36 +0800

    Add same position judging and crash detection.

--- ThreeBody.cpp
+++ ThreeBody.cpp
@@ -3,6 +3,7 @@
 
 using namespace std;
 const double G = 6.67408e-11;
+const double MIN_DIS = 1e-6;
 
 struct R3 {
 	double x, y, z;
@@ -54,35 +55,42 @@ struct R3 {
 struct Body {
 	R3 s, v, a;
 	double m;
+
+	inline bool operator==(Body s1) {
+		R3 ds = this->s - s1.s;
+		double dis = sqrt(ds * ds);
+		return dis <= MIN_DIS;
+	}
 };
 Body *star;
 int n;
 double per, t, display, kper;
 
-void updatea()
+void update_a()
 {
-	R3 inita(0, 0, 0);
-	for (int i = 0; i < n; i++) star[i].a = inita;
+	R3 init_a;
+	for (int i = 0; i < n; i++) star[i].a = init_a;
 	
 	for (int i = 0; i < n; i++) {
 		for (int j = i + 1; j < n; j++) {
-			R3 tvct = star[j].s - star[i].s;
-			double dis = sqrt(tvct * tvct);
-			tvct = tvct * G / (dis * dis * dis);
-			star[i].a += tvct * star[j].m;
-			star[j].a += -tvct * star[i].m;
+			R3 ds = star[j].s - star[i].s;
+			double ds2 = ds * ds;
+			double dis = sqrt(ds2);
+			ds *= G / (ds2 * dis);
+			star[i].a += ds * star[j].m;
+			star[j].a += -ds * star[i].m;
 		}
 	}
 }
 
-void updatev()
+void update_v()
 {
 	for (int i = 0; i < n; i++) {
 		star[i].v += star[i].a * per;
 	}
 }
 
-void updates()
+void update_s()
 {
 	for (int i = 0; i < n; i++) {
 		star[i].s += star[i].v * per + star[i].a * kper;
@@ -101,41 +109,66 @@ void displays(double time)
 	cout << endl;
 }
 
+bool is_close(int last)
+{
+	for (int i = 0; i < last - 1; i++)
+		if (star[i] == star[last]) return true;
+	return false;
+}
+
+bool is_crashed()
+{
+	for (int i = 1; i < n; i++)
+		if (is_close(i)) return true;
+	return false;
+}
+
 int main()
 {
-	cout << "Number of stars:";
+	cout << "Number of stars: ";
 	cin >> n;
-	cout << "Time interval for calculating(sec):";
+	cout << "Calculation interval(sec): ";
 	cin >> per;
-	cout << "Finish time(sec):";
+	cout << "Finish time(sec): ";
 	cin >> t;
-	cout << "Time interval for display(sec):";
+	cout << "Display interval(sec): ";
 	cin >> display;
 	
 	star = new Body[n];
 	if (!star) {
-		cout << "Initializing failed." << endl;
+		cout << "Initializing failed!" << endl;
 		return 0;
 	}
 	
-	cout << endl; 
+	cout << endl;
 	for (int i = 0; i < n; i++) {
 		cout << "Star " << i << ':' << endl;
-		cout << "Mass:";
+		cout << "Mass: ";
 		cin >> star[i].m;
-		cout << "Position:";
+		cout << "Position: ";
 		cin >> star[i].s.x >> star[i].s.y >> star[i].s.z;
-		cout << "Velocity:";
+		cout << "Velocity: ";
 		cin >> star[i].v.x >> star[i].v.y >> star[i].v.z;
 		cout << endl;
+
+		if (is_close(n)) {
+			cout << "Position cannot be the same or too close!" << endl;
+			return 0;
+		}
 	}
 	
-	kper = 0.5 * per * per;
+	kper = 0.5 * per * per;		// Reduce calc complexation
 	double interval = per;
 	 for (double now = per; now <= t || interval >= display; now += per)  {
-	 	updatea();
-	 	updates();
-	 	updatev();
+	 	update_a();
+	 	update_s();
+	 	update_v();
+
+		// Annotate this if you don't want crash detection
+		if (is_crashed()) {
+			cout << "Crashed!" << endl;
+			return 0;
+		}
 	 	
 	 	if (interval >= display) {
 			interval = 0;
